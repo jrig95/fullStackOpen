9f0dbd01d55b2eec6b7752603d69888038705b13
James Riggleman <james.riggleman@yahoo.com>
2023-08-22 17:45:20 +0800

    created build for frontend and updated baseUrl for production and development

--- part2/the-phonebook/src/services/persons.js
+++ part2/the-phonebook/src/services/persons.js
@@ -1,5 +1,11 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/persons'
+let baseUrl = '';
+if (process.env.NODE_ENV === 'development') {
+    baseUrl = 'http://localhost:3001/api/persons';
+} else {
+    baseUrl = '/api/persons';
+}
+
 
 const getAll = () => {
   const request = axios.get(baseUrl)
